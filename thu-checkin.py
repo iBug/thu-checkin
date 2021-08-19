93b2771c838c68b7384126ad134d557927f44c00
iBug <git@ibugone.com>
2021-08-19 23:08:29 +0800

    Code before CAS requires CAPTCHA

--- thu-checkin.py
+++ thu-checkin.py
@@ -15,12 +15,18 @@ from requests.adapters import HTTPAdapter
 
 print("Tsinghua University Daily Health Report")
 
+print("Waiting for IPv6 access", end="")
 while True:
     try:
         requests.get("http://test6.ustc.edu.cn/", timeout=1)
     except requests.exceptions.ConnectionError:
+        print(end=".")
         time.sleep(1)
     else:
+        print()
+        r = requests.get("http://[2001:da8:d800::10]/cgi-bin/myip", timeout=5)
+        if r.status_code == 200:
+            print("IPv6 address:", re.search("'(.*?)'", r.text)[1])
         break
 
 dirname = os.path.dirname(os.path.realpath(__file__))
@@ -36,6 +42,8 @@ province = data['PROVINCE']
 city = data["CITY"]
 country = data["COUNTRY"]
 is_inschool = data.get("IS_INSCHOOL", "2")
+hmac_secret = data.get("HMAC_SECRET", "").encode()
+post_url = data.get("POST_URL")
 
 # 1: 在校园内, 2: 正常在家
 now_status = "2" if is_inschool == "0" else "1"
@@ -54,7 +62,7 @@ retries = Retry(total=5,
 
 s = requests.Session()
 s.mount("https://", HTTPAdapter(max_retries=retries))
-s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67"
 s.get(CAS_LOGIN_URL, params={"service": CAS_RETURN_URL})
 
 data = {
@@ -83,6 +91,7 @@ data = {
     "now_country": country,
     "gps_country": "",
     "now_detail": "",
+    "is_inschool": "6",
     "body_condition": "1",
     "body_condition_detail": "",
     "now_status": now_status,
@@ -93,13 +102,12 @@ data = {
     "last_touch_sars_detail": "",
     "is_danger": "0",
     "is_goto_danger": "0",
+    "jinji_lxr": "\uFFFD",
+    "jinji_guanxi": "\uFFFD",
+    "jiji_mobile": "\uFFFD",
     "other_detail": "\uFFFD",
     # https://twitter.com/tenderlove/status/722565868719177729
 }
-
-# Set "in_school" only when located in the right city
-if province == "340000" and city == "340100":
-    data["is_inschool"] = is_inschool
 
 r = s.post(REPORT_URL, data=data, headers={"Referer": r.url})
 with open(os.path.join(dirname, "last.html"), "wb") as f:
@@ -109,15 +117,16 @@ with open(os.path.join(dirname, "last.html"), "wb") as f:
 r.raise_for_status()
 
 if r.text.find("上报成功") >= 0:
+    print("Checkin success")
     payload = json.dumps({"type": "checkin"})
 else:
+    print("Checkin failed")
     payload = json.dumps({"type": "checkin-fail"})
-signature = hmac.new(b"REDACTED", payload.encode("utf-8"), hashlib.sha1).hexdigest()
+signature = hmac.new(hmac_secret, payload.encode("utf-8"), hashlib.sha1).hexdigest()
 headers = {
     "Content-Type": "application/json",
     "X-GitHub-Event": "REDACTED",
     "X-Hub-Signature": f"sha1={signature}",
 }
-r = requests.post("https://www.example.com/", headers=headers, data=payload)
-print(r)
-print(r.text)
+r = requests.post(post_url, headers=headers, data=payload)
+#print("Notification:", r.status_code)
